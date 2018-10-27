6cd65628968e28c0184e0594fb1059e55c9ca3de
Amit Patange <amitpatange88@gmail.com>
2018-10-27 13:58:24 +0530

    client secretes loaded from other file

--- HostLive/HostLive.svc.cs
+++ HostLive/HostLive.svc.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -10,14 +11,20 @@ using MsgBroker = RabbitMQ;
 
 namespace HostLive
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "HostLive" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select HostLive.svc or HostLive.svc.cs at the Solution Explorer and start debugging.
+    /// <summary>
+    /// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "HostLive" in code, svc and config file together.
+    /// NOTE: In order to launch WCF Test Client for testing this service, please select HostLive.svc or HostLive.svc.cs at the Solution Explorer 
+    /// and start debugging.
+    /// </summary>
     public class HostLive : IHostLive
     {
+        private static string FilePathSecrets = @"C:\ServiceLogs\keys\HostLiveSecrets.txt";
         private static int SystemContinuosOnCount = 1;
         private static DateTime LastRecordedTime;
         public delegate void _CallbackConsumerDel(string message);
         public static _CallbackConsumerDel _callback = new _CallbackConsumerDel(HostLive.DoOnConsumeMessages);
+
+
         /// <summary>
         /// Set Entry point : First Route method which will check below things.
         /// 1. On System start we will insert log in to C:\ServiceLogs\HostLive.txt
@@ -50,17 +57,17 @@ namespace HostLive
                 string systemLog = SerializeJSONData(s1);
                 IsDone = HostFileUtility.Instance.WriteLog(systemLog);
 
-                //Call RabbitMQ as message broker.
-                using (MsgBroker.RabbitMQ Rpc = new MsgBroker.RabbitMQ())
-                {
-                    if (Internet.IsConnectionActive())
-                    {
-                        //Rpc.MessageBrokerConsume();
-                        SendEmail(s1);
-                    }
-                    else
-                    {
 
+                if (Internet.IsConnectionActive())
+                {
+                    //Consume RabbitMQ messages here somewhere.
+                    SendEmail(s1);
+                }
+                else
+                {
+                    //Call RabbitMQ as message broker.
+                    using (MsgBroker.RabbitMQ Rpc = new MsgBroker.RabbitMQ())
+                    {
                         Rpc.MessageBrokerPublish(systemLog);
                     }
                 }
@@ -75,9 +82,21 @@ namespace HostLive
             return IsDone;
         }
 
+        private string ReadServerSecrets()
+        {
+            string secrets = string.Empty;
+            using (var fileStream = new FileStream(FilePathSecrets, FileMode.Open, FileAccess.Read))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                secrets = streamReader.ReadToEnd();
+            }
+
+            return secrets;
+        }
+
         public static void DoOnConsumeMessages(string message)
         {
-            var details = DeserializeJSONData(message);
+            var details = DeserializeJSONData<SystemDetails>(message);
             Console.WriteLine(" [x] Received {0}", message);
         }
 
@@ -89,12 +108,18 @@ namespace HostLive
         private bool SendEmail(SystemDetails s1)
         {
             //prepare to send an email with login creds
+            string Secrets = ReadServerSecrets();
+
+            if (string.IsNullOrWhiteSpace(Secrets))
+                return false;
+
+            var Keys = DeserializeJSONData<Secrets>(Secrets);
 
             string Subject = GetSubjectLine();
             string Body = GetBody(s1);
 
             Mail m = new Mail();
-            m.Send("smtp.gmail.com", "amitpatange88@gmail.com", "b21rYXJhdHJ0cnRy", "amitpatange88@gmail.com", Subject, Body);
+            m.Send("smtp.gmail.com", Keys.HostLiveSecrets.Gmail.username, Keys.HostLiveSecrets.Gmail.password, Keys.HostLiveSecrets.Gmail.username, Subject, Body);
 
             return true;
         }
@@ -102,6 +127,86 @@ namespace HostLive
         private string GetSubjectLine()
         {
             return string.Format("HostLive Notification : Your system is booted up and running since {0}+ hours.", SystemContinuosOnCount);
+        }
+
+        private SystemDetails GetSystemDetails()
+        {
+            SystemDetails s = new SystemDetails()
+            {
+                LogId = Guid.NewGuid(),
+                PreciseTimeStamp = DateTime.Now,
+                UTCTimeStamp = DateTime.UtcNow,
+                MachineName = System.Environment.MachineName,
+                OSVersion = System.Environment.OSVersion.ToString(),
+                Is64BitOperatingSystem = System.Environment.Is64BitOperatingSystem,
+                ProcessorCount = System.Environment.ProcessorCount,
+                OnCount = SystemContinuosOnCount,
+                ProcessesRunning = ProcessesRunningForLogs()
+            };
+
+            return s;
+        }
+
+        
+        private string SerializeJSONData(SystemDetails e)
+        {
+            Newtonsoft.Json.JsonSerializerSettings jss = new Newtonsoft.Json.JsonSerializerSettings();
+
+            Newtonsoft.Json.Serialization.DefaultContractResolver dcr = new Newtonsoft.Json.Serialization.DefaultContractResolver();
+            dcr.DefaultMembersSearchFlags |= System.Reflection.BindingFlags.NonPublic;
+            jss.ContractResolver = dcr;
+
+            var response = Newtonsoft.Json.JsonConvert.SerializeObject(e, jss);
+
+            return response;
+        }
+
+        /// <summary>
+        /// Serialize the JSON data. Using with Default contract resolver for non public fields.
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        private static dynamic DeserializeJSONData<T>(string message)
+        {
+            Newtonsoft.Json.JsonSerializerSettings jss = new Newtonsoft.Json.JsonSerializerSettings();
+
+            Newtonsoft.Json.Serialization.DefaultContractResolver dcr = new Newtonsoft.Json.Serialization.DefaultContractResolver();
+            dcr.DefaultMembersSearchFlags |= System.Reflection.BindingFlags.NonPublic;
+            jss.ContractResolver = dcr;
+
+            var response = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(message);
+
+            return response;
+        }
+
+        private string ProcessesRunningAttachInEmail()
+        {
+            string processesRunning = string.Empty;
+            System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcesses();
+            foreach (System.Diagnostics.Process p in processes)
+            {
+                if (!String.IsNullOrEmpty(p.MainWindowTitle))
+                {
+                    processesRunning += p.MainWindowTitle + "<br>-----------------------------------<br>";
+                }
+            }
+
+            return processesRunning;
+        }
+
+        private string ProcessesRunningForLogs()
+        {
+            string processesRunning = string.Empty;
+            System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcesses();
+            foreach (System.Diagnostics.Process p in processes)
+            {
+                if (!String.IsNullOrEmpty(p.MainWindowTitle))
+                {
+                    processesRunning += p.MainWindowTitle + "&nbsp;";
+                }
+            }
+
+            return processesRunning;
         }
 
         private string GetBody(SystemDetails s1)
@@ -159,85 +264,6 @@ namespace HostLive
 
 
             return body;
-        }
-
-        private SystemDetails GetSystemDetails()
-        {
-            SystemDetails s = new SystemDetails()
-            {
-                LogId = Guid.NewGuid(),
-                PreciseTimeStamp = DateTime.Now,
-                UTCTimeStamp = DateTime.UtcNow,
-                MachineName = System.Environment.MachineName,
-                OSVersion = System.Environment.OSVersion.ToString(),
-                Is64BitOperatingSystem = System.Environment.Is64BitOperatingSystem,
-                ProcessorCount = System.Environment.ProcessorCount,
-                OnCount = SystemContinuosOnCount,
-                ProcessesRunning = ProcessesRunningForLogs()
-            };
-
-            return s;
-        }
-
-        /// <summary>
-        /// Serialize the JSON data. Using with Default contract resolver for non public fields.
-        /// </summary>
-        /// <param name="e"></param>
-        /// <returns></returns>
-        private string SerializeJSONData(SystemDetails e)
-        {
-            Newtonsoft.Json.JsonSerializerSettings jss = new Newtonsoft.Json.JsonSerializerSettings();
-
-            Newtonsoft.Json.Serialization.DefaultContractResolver dcr = new Newtonsoft.Json.Serialization.DefaultContractResolver();
-            dcr.DefaultMembersSearchFlags |= System.Reflection.BindingFlags.NonPublic;
-            jss.ContractResolver = dcr;
-
-            var response = Newtonsoft.Json.JsonConvert.SerializeObject(e, jss);
-
-            return response;
-        }
-
-        private static SystemDetails DeserializeJSONData(string message)
-        {
-            Newtonsoft.Json.JsonSerializerSettings jss = new Newtonsoft.Json.JsonSerializerSettings();
-
-            Newtonsoft.Json.Serialization.DefaultContractResolver dcr = new Newtonsoft.Json.Serialization.DefaultContractResolver();
-            dcr.DefaultMembersSearchFlags |= System.Reflection.BindingFlags.NonPublic;
-            jss.ContractResolver = dcr;
-
-            var response = Newtonsoft.Json.JsonConvert.DeserializeObject<SystemDetails>(message);
-
-            return response;
-        }
-
-        private string ProcessesRunningAttachInEmail()
-        {
-            string processesRunning = string.Empty;
-            System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcesses();
-            foreach (System.Diagnostics.Process p in processes)
-            {
-                if (!String.IsNullOrEmpty(p.MainWindowTitle))
-                {
-                    processesRunning += p.MainWindowTitle + "<br>-----------------------------------<br>";
-                }
-            }
-
-            return processesRunning;
-        }
-
-        private string ProcessesRunningForLogs()
-        {
-            string processesRunning = string.Empty;
-            System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcesses();
-            foreach (System.Diagnostics.Process p in processes)
-            {
-                if (!String.IsNullOrEmpty(p.MainWindowTitle))
-                {
-                    processesRunning += p.MainWindowTitle + "&nbsp;";
-                }
-            }
-
-            return processesRunning;
         }
     }
 }
---
+++ HostLive/HostLiveSecrets.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HostLive
+{
+    public class HostLiveSecrets
+    {
+        public string project { get; set; }
+        public string Description { get; set; }
+        public Gmail Gmail { get; set; }
+        public Yahoo Yahoo { get; set; }
+    }
+
+    public class Gmail
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+
+    public class Yahoo
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+
+    public class RabbitMQ
+    {
+        public string queue { get; set; }
+    }
+
+    public class Designpattern
+    {
+        public string name { get; set; }
+    }
+
+    public class Secrets
+    {
+        public HostLiveSecrets HostLiveSecrets { get; set; }
+        public IList<RabbitMQ> RabbitMQ { get; set; }
+        public Designpattern designpattern { get; set; }
+    }
+
+}
